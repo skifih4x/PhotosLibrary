ab77da9b6b808151808036e6cff42161d40e5921
artem <arte@kb.ri>
2022-08-04 22:56:14 +0300

    project is done!

---
+++ PhotosLibrary/PhotoCell.swift
@@ -0,0 +1,82 @@
+//
+//  PhotoCell.swift
+//  PhotosLibrary
+//
+//  Created by Артем Орлов on 22.07.2022.
+//
+
+import UIKit
+import SDWebImage
+
+class PhotoCell: UICollectionViewCell {
+    
+    static let reusedId = "PhotoCell"
+    
+    private let checkMark: UIImageView = {
+        let image = UIImage(named: "bird1")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.alpha = 0
+        return imageView
+    }()
+    
+     let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    var unsplashPhoto: UnsplashPhoto! {
+        didSet {
+            let photoUrl = unsplashPhoto.urls["regular"]
+            guard let imageUrl = photoUrl, let url = URL(string: imageUrl) else { return }
+            photoImageView.sd_setImage(with: url, completed: nil  )
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            updateSelectedState()
+        }
+    }
+    
+
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        photoImageView.image = nil
+    }
+    
+    private func updateSelectedState() {
+        photoImageView.alpha = isSelected ? 0.7 : 1
+        checkMark.alpha = isSelected ? 1 : 0
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        updateSelectedState()
+        setUpPhotoImageView()
+        setUpCheckmarkView()
+    }
+    
+    private func setUpPhotoImageView() {
+        addSubview(photoImageView)
+        photoImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        photoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        photoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+    }
+    
+    private func setUpCheckmarkView() {
+        addSubview(checkMark)
+        checkMark.trailingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: -8).isActive = true
+        checkMark.bottomAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: -8).isActive = true
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- PhotosLibrary/PhotosCollectionViewController.swift
+++ PhotosLibrary/PhotosCollectionViewController.swift
@@ -11,6 +11,13 @@ class PhotosCollectionViewController: UICollectionViewController {
     
     private var timer: Timer?
     
+    private var photos = [UnsplashPhoto]()
+    
+    private var selectedImages = [UIImage]()
+    
+    private let itemPerRow: CGFloat = 2
+    private let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    
     private lazy var addBarButtonItem: UIBarButtonItem = {
         return UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonTapped))
     }()
@@ -19,13 +26,28 @@ class PhotosCollectionViewController: UICollectionViewController {
         return UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(actionBarButtonTapped))
     }()
     
+    private var numberOfSelectedPhotos: Int {
+        collectionView.indexPathsForSelectedItems?.count ?? 0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.backgroundColor = .orange
+        updateNavigationButtonState()
+        collectionView.backgroundColor = .white
         setupNavigationBar()
         setupCollectionView()
         setupSearchBar()
+    }
+    
+    private func updateNavigationButtonState() {
+        addBarButtonItem.isEnabled = numberOfSelectedPhotos > 0
+        actionBarButtonItem.isEnabled = numberOfSelectedPhotos > 0
+    }
+    
+    func refresh() {
+        self.selectedImages.removeAll()
+        self.collectionView.selectItem(at: nil, animated: true, scrollPosition: [])
+        updateNavigationButtonState()
     }
     
     //MARK: - NavigationItem action
@@ -34,13 +56,30 @@ class PhotosCollectionViewController: UICollectionViewController {
         print(#function)
     }
     
-    @objc private func actionBarButtonTapped() {
+    @objc private func actionBarButtonTapped(sender: UIBarButtonItem) {
         print(#function)
+        
+        let sharedController = UIActivityViewController(activityItems: selectedImages, applicationActivities: nil)
+        
+        sharedController.completionWithItemsHandler = { _, bool, _, _ in
+            if bool {
+                self.refresh()
+            }
+        }
+        
+        sharedController.popoverPresentationController?.barButtonItem = sender
+        sharedController.popoverPresentationController?.permittedArrowDirections = .any
+        present(sharedController, animated: true)
     }
     
     //MARK: - Setup UI Elements
     private func setupCollectionView() {
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
+        collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.reusedId)
+        
+        collectionView.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        collectionView.contentInsetAdjustmentBehavior = .automatic
+        collectionView.allowsMultipleSelection = true
     }
     
     private func setupNavigationBar() {
@@ -64,13 +103,30 @@ class PhotosCollectionViewController: UICollectionViewController {
     //MARK: - UICollectionViewDataSource, UICollectionViewDelegate
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        5
+        photos.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
-        cell.backgroundColor = .red
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.reusedId, for: indexPath) as! PhotoCell
+        let unspashPhoto = photos[indexPath.item]
+        cell.unsplashPhoto = unspashPhoto
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        updateNavigationButtonState()
+        guard let cell = collectionView.cellForItem(at: indexPath) as? PhotoCell else { return }
+        guard let image = cell.photoImageView.image else { return }
+            selectedImages.append(image)
+        
+    }
+    override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        updateNavigationButtonState()
+        guard let cell = collectionView.cellForItem(at: indexPath) as? PhotoCell else { return }
+        guard let image = cell.photoImageView.image else { return }
+        if let index = selectedImages.firstIndex(of: image) {
+            selectedImages.remove(at: index)
+        }
     }
 }
 
@@ -81,12 +137,37 @@ extension PhotosCollectionViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
+        timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { _ in
-            NetworkDataFetcher.shared.fetchImages(searchTerm: searchText) { searchResults in
-                searchResults?.results.map({ photo in
-                    print(photo.urls["small"])
-                })
+            NetworkDataFetcher.shared.fetchImages(searchTerm: searchText) { [weak self ] searchResults in
+                guard let fetchedPhotos = searchResults else { return }
+                self?.photos = fetchedPhotos.results
+                self?.collectionView.reloadData()
+                self?.refresh()
             }
         })
     }
+}
+
+//MARK: - UICollectionViewDelegateFlowLayout
+
+extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let photo = photos[indexPath.item]
+        let paddingSpace = sectionInserts.left * (itemPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemPerRow
+        let height = CGFloat(photo.height) * widthPerItem / CGFloat(photo.width)
+        return CGSize(width: widthPerItem, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        sectionInserts
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        sectionInserts.left
+    }
+    
 }
